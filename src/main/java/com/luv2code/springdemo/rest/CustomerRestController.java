8fc58d6433685bf4ae6c7f97acb9aed3c2608d41
Ivanncho <jovanovski.ivanco@yahoo.com>
2021-04-12 12:31:05 +0200

    Implementing Update Customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,14 @@ public class CustomerRestController {
 		
 		customerService.saveCustomer(customer);
 		
+		return customer;
+	}
+	
+	//update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
+		
+		customerService.saveCustomer(customer);
 		return customer;
 	}
 	
